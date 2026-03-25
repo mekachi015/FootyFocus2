35e623050dddf8cb712b4fa6090d16f0e1e467ff
mekachi015 <katlegomakoti@gmail.com>
2026-03-25 22:55:26 +0200

    project initialized

---
+++ Backend/FootyFocus2/src/main/java/com/example/FootyFocus2/FootyFocus2Application.java
@@ -0,0 +1,13 @@
+package com.example.FootyFocus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootyFocus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootyFocus2Application.class, args);
+	}
+
+}
---
+++ Backend/FootyFocus2/src/test/java/com/example/FootyFocus2/FootyFocus2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FootyFocus2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootyFocus2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
